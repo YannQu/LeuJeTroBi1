9ecd734273f47561c18332eda6a7f8b1f8a70b43
Lecuyer johan <johan.lecuyer4@gmail.com>
2018-09-25 11:03:17 +0200

    modif sql

--- php/game.php
+++ php/game.php
@@ -13,3 +13,21 @@ if (isset($_POST['niveau_manche']))
 {
     $niveau_manche = $_POST['niveau_manche'];
 }
+$req = $pdo->prepare('SELECT * FROM campagne WHERE niveau = :niveau_manche');
+$req->execute(['niveau_manche' => $niveau_manche]);
+$manche = $req->fetch(PDO::FETCH_OBJ);
+
+
+$req = $pdo->prepare('SELECT * FROM ennemi WHERE id_ennemi = :id_ennemi');
+$req->execute(['id_ennemi' => $manche->id_ennemi]);
+$ennemi = $req->fetch(PDO::FETCH_OBJ);
+
+
+$req = $pdo->prepare('SELECT * FROM equipement AS e INNER JOIN relation_equipement_campagne AS r 
+ON r.id_equipement = e.id_equipement WHERE r.id_campagne = :id_campagne');
+$req->execute(['id_campagne' => $manche->id_campagne]);
+$equipement = $req->fetch(PDO::FETCH_OBJ);
+
+
+
+

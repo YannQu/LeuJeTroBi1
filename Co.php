cb7065c454c74acdd1be90471bee69b1f1eb4a7f
Lecuyer johan <johan.lecuyer4@gmail.com>
2018-09-26 09:27:16 +0200

    endgame start

--- Co.php
+++ Co.php
@@ -4,9 +4,10 @@ if (isset($_GET['insc']) && $_GET['insc'] == 'yes') {
 	$template = 'createChar';
 }else if(isset($_GET['cmpgn']) && $_GET['cmpgn'] == "yes"){
 	$template = 'game';
+}else if(isset($_GET['endgame']) && $_GET['endgame'] == "yes"){
+	$template = 'endgame';
 }else{
 	$template = 'character';
 }
-$template='game';
 include 'layout/layout.phtml';
 

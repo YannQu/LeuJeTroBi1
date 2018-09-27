6a643645d621505e6dcae11faba17e74dbc8dd72
Lecuyer johan <johan.lecuyer4@gmail.com>
2018-09-27 13:33:49 +0200

    fichier sql

--- php/getCharacter.php
+++ php/getCharacter.php
@@ -9,16 +9,8 @@ $req->execute(['id_user' => $user->id_utilisateur]);
 $character = $req->fetch(PDO::FETCH_OBJ);
 //var_dump($character);
 
-$req = $pdo->prepare('SELECT * FROM inventaire WHERE id_personnage = :id_user');
+$req = $pdo->prepare('SELECT * FROM inventaire AS i INNER JOIN equipement AS e ON i.id_equipement = e.id_equipement WHERE i.id_personnage = :id_user');
 $req->execute(['id_user' => $character->id_personnage]);
-$inventaire = $req->fetchAll(PDO::FETCH_OBJ);
-
-$detailsInv = array();
-$cmpt = 0;
-foreach ($inventaire as $i){
-	//var_dump($i);
-    $req = $pdo->prepare('SELECT * FROM equipement WHERE id_equipement = :id_equip');
-	$req->execute(['id_equip' => $i->id_equipement]);
-	array_push($detailsInv, $req->fetch(PDO::FETCH_OBJ));
-}
+$detailsInv = $req->fetchAll(PDO::FETCH_OBJ);
+$cmpt=0;
 //var_dump($detailsInv);
